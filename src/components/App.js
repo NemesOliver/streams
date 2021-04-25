d80e29c030e8952026fe355ad0eb83421711b36a
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-04-25 19:00:00 +0100

    Header

--- src/components/App.js
+++ src/components/App.js
@@ -6,12 +6,14 @@ import StreamList from "./streams/StreamList";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamCreate from "./streams/StreamCreate";
+import Header from "./Header";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
