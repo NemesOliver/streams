0126a768858da68e662abd99592a33ddb6f873c2
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-04-29 12:55:42 +0100

    making modal reusable

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,13 +1,30 @@
 import React from "react";
+import history from "../../history";
 import Modal from "../Modal";
 
-const StreamDelete = () => {
-  return (
-    <div>
-      Stream Delete
-      <Modal />
-    </div>
-  );
-};
+class StreamDelete extends React.Component {
+  renderActions() {
+    return (
+      <React.Fragment>
+        <button className="ui negative button">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Stream Delete
+        <Modal
+          onDismiss={() => history.push("/")}
+          actions={this.renderActions()}
+          header="Delete Stream"
+          content="Are you sure you want to Delete this stream?"
+        />
+      </div>
+    );
+  }
+}
 
 export default StreamDelete;
