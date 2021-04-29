2f2f956425800c9cc6f48e601b2be82e7abab073
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-04-29 13:36:43 +0100

    delete streem

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,30 +1,57 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import history from "../../history";
 import Modal from "../Modal";
+import { fetchStream, deleteStream } from "../../actions";
 
 class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
   renderActions() {
+    const { id } = this.props.match.params;
+
     return (
       <React.Fragment>
-        <button className="ui negative button">Delete</button>
-        <button className="ui button">Cancel</button>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui negative button"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
       </React.Fragment>
     );
   }
 
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to Delete this stream?";
+    }
+
+    return `Are you sure you want to Delete ${this.props.stream.title}?`;
+  }
+
   render() {
     return (
-      <div>
-        Stream Delete
-        <Modal
-          onDismiss={() => history.push("/")}
-          actions={this.renderActions()}
-          header="Delete Stream"
-          content="Are you sure you want to Delete this stream?"
-        />
-      </div>
+      <Modal
+        onDismiss={() => history.push("/")}
+        actions={this.renderActions()}
+        header="Delete Stream"
+        content={this.renderContent()}
+      />
     );
   }
 }
 
-export default StreamDelete;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
